96101104ca61ddb16017b649d469df9ec64c2a65
SuperBluestar <qkrwntjd116**>
2021-07-03 23:14:41 +0900

    fix display date

--- resources/views/calendar/undemolition.blade.php
+++ resources/views/calendar/undemolition.blade.php
@@ -42,9 +42,11 @@
                                                         <a @if ($isCharge) href="{{ route('charge.yetproject.register', $project->id) }}" @else href="{{ route('yetproject.register', $project->id) }}" @endif>
                                                             <span class="title">{{ $project->name }}</span><br>
                                                             <div class="spent_date" style="display:table-cell; text-align:right;">
-                                                                <span class="colorRed bold">経過日数
-                                                                {{ $project->work_on && (strtotime("now") >= strtotime($project->work_on)) ? ceil((strtotime("now") - strtotime($project->work_on))/3600/24) : "" }}
-                                                                日</span>
+                                                                @if ($project->work_on && (strtotime("now") >= strtotime($project->work_on)))
+	                                                                <span class="colorRed bold">経過日数
+	                                                                {{ ceil((strtotime("now") - strtotime($project->work_on))/3600/24) }}
+		                                                            日</span>
+	                                                            @endif
                                                             </div>
                                                             <ul class="flex__wrap f__start status">
                                                                 <li class="@if ($project->is_surveyed) done @endif"><span>現調</span></li>
